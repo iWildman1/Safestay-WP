c5756b7d7f5f0117629603e125b4a7658a9dee22
Daniel Wildman <dan@Daniels-MacBook-Pro.local>
2018-07-18 15:01:54 +0100

    Added dynamic explore section

--- template-parts/explore-spain.php
+++ template-parts/explore-spain.php
@@ -1,57 +1,127 @@
-<div class="container" data-country="Spain">
-        <div class="row">
-            <div class="grid-item grid-30 bg-spain flex centralize">
-                <div class="col-inner">
-                    <h1>Spain.</h1>
-                    <p>Beautiful destinations located within the heart of Barcelona and Madrid</p>
-                </div>
-            </div>
-            <div class="grid-item third bg-spain-image flex centralize" style="background-image: url('<?php bloginfo('stylesheet_directory') ?>/dist/img/411x364_Passeig.png')">
-                 <h3>Passeig de Gràcia</h3>
-            </div>
-            <div class="grid-item grid-375 vertical-split no-padding">
-                <div class="vertical-half offers">
-                    <a href="#"> <img src="<?php bloginfo('stylesheet_directory') ?>/dist/img/pounds.png" alt=""> See offers </a>
-                </div>
-                <div class="vertical-half offers">
-                    <a href="#"><img src="<?php bloginfo('stylesheet_directory') ?>/dist/img/things-icon.png" alt=""> #Thingstodo</a>
-                </div>
-            </div>
-        </div>
-        <div class="slider-wrapper">
-            <div class="row explore-slider" style="height: 34rem">
-                <div class="grid-item half bg-spain-image flex centralize" style="background-image: url('<?php bloginfo('stylesheet_directory') ?>/dist/img/600x329_Barcelona_Sea.png')">
-                    <h3>Barcelona Sea</h3>
-                </div>
-                <div class="grid-item grid-225 bg-spain-image flex centralize" style="background-image: url('<?php bloginfo('stylesheet_directory') ?>/dist/img/269x329_Madrid.png')">
-                    <h3>Madrid</h3>
-                </div>
-                <div class="grid-item grid-275 bg-spain-image flex centralize no-margin-right" style="background-image: url('<?php bloginfo('stylesheet_directory') ?>/dist/img/349x329_Barcelona_Gothic.png')">
-                    <h3>Barcelona Gothic</h3>
-                </div>
-                <div class="grid-item half bg-spain-image flex centralize" style="background-image: url('<?php bloginfo('stylesheet_directory') ?>/dist/img/600x329_Barcelona_Sea.png')">
-                    <h3>Barcelona Sea</h3>
-                </div>
-                <div class="grid-item grid-225 bg-spain-image flex centralize" style="background-image: url('<?php bloginfo('stylesheet_directory') ?>/dist/img/269x329_Madrid.png')">
-                    <h3>Madrid</h3>
-                </div>
-                <div class="grid-item grid-275 bg-spain-image flex centralize no-margin-right" style="background-image: url('<?php bloginfo('stylesheet_directory') ?>/dist/img/349x329_Barcelona_Gothic.png')">
-                    <h3>Barcelona Gothic</h3>
-                </div>
-            </div>
-        </div>
+<?php 
 
-        <div class="slider-controls">
+    $args = array(
+        'taxonomy' => 'locations',
+        'hide_empty' => false,
+    );
+    $terms = get_terms($args);
 
-        </div>
-        <div class="drag-info">
-            <div class="drag-img">
-                <img src="<?php bloginfo('stylesheet_directory') ?>/dist/img/drag-img.png" alt="">
+
+    if ( $terms && !is_wp_error( $terms ) ) :
+        $i = 0;
+        foreach ( $terms as $term ) :
+            if ( $term->slug == "spain" ) {
+
+                ?>
+                
+                <div class="container" data-country="Spain">
+                    <div class="row">
+                        <div class="grid-item grid-30 bg-spain flex centralize">
+                                <div class="col-inner">
+                                    <h1><?php echo $term->name ?></h1>
+                                    <p><?php echo $term->description ?></p>
+                                </div>
+                            </div>
+                            <?php 
+                            
+                            $query = new WP_Query( array(
+                                'post_type' => 'hostel',
+                                'posts_per_page' => 1,
+                                    'tax_query' => array(
+                                        array(
+                                            'taxonomy' => 'locations',
+                                            'field' => 'slug',
+                                            'terms' => $term->slug
+                                        )
+                                    )
+                            ) );
+
+                            if ( $query->have_posts() ) :
+                                while ( $query->have_posts() ) : $query->the_post();
+                                    ?>
+                                    <div class="grid-item third bg-uk-image flex centralize" style="background-image: url('<?php echo get_the_post_thumbnail_url() ?>')">
+                                        <h3 class="underline-yellow"><?php the_title() ?></h3>
+                                    </div>
+                                    <?php
+                                endwhile;   
+                            endif;
+                            
+                            ?>
+                            
+                            <div class="grid-item grid-375 vertical-split no-padding">
+                                <div class="vertical-half offers">
+                                    <a href="#">
+                                        <img src="<?php bloginfo('stylesheet_directory') ?>/dist/img/pounds.png" alt=""> See offers </a>
+                                </div>
+                                <div class="vertical-half offers">
+                                    <a href="#">
+                                        <img src="<?php bloginfo('stylesheet_directory') ?>/dist/img/things-icon.png" alt=""> #Thingstodo</a>
+                                </div>
+                            </div>
+                    </div>
+
+                    <?php 
+                    
+                    $query = new WP_Query( array(
+                        'post_type' => 'hostel',
+                        'tax_query' => array(
+                            array(
+                                'taxonomy' => 'locations',
+                                'field' => 'slug',
+                                'terms' => $term->slug
+                            )
+                        )
+                    ) );
+
+                    if ( $query->have_posts() && $query->found_posts > 1 ) :
+                        $i = 0;
+                        ?>
+                            <div class="slider-wrapper">
+                                <div class="row explore-slider" style="height: 34rem">
+
+                        <?php
+                        while ( $query->have_posts() ) : $query->the_post();
+                            if ($i == 0) {
+                                echo '';
+                            } else {
+                                ?>
+                                    <div class="grid-item <?php echo get_field('size_in_slider') ?> bg-spain-image flex centralize" style="background-image: url('<?php echo get_the_post_thumbnail_url() ?>')">
+                                        <h3 class="underline-yellow"><?php the_title() ?></h3>
+                                    </div>
+                                <?php
+                            }
+                            $i++;
+                        endwhile;
+                        ?>
+                                </div>
+                            </div>
+                            <div class="slider-controls">
+
+                            </div>
+                            <div class="drag-info">
+                                <div class="drag-img">
+                                    <img src="<?php bloginfo('stylesheet_directory') ?>/dist/img/drag-img.png" alt="">
+                                </div>
+                                <div class="drag-text">
+                                    <p>
+                                        Drag to Explore
+                                    </p>
+                                </div>
+                            </div>
+                            
+                        <?php
+                    endif;
+                    
+                    ?>
+
+                <?php
+
+            }
+            ?>
             </div>
-            <div class="drag-text">
-                <p>
-                    Drag to Explore
-                </p>
-            </div>
-        </div>
-    </div>
+            <?php
+            $i++;
+        endforeach;
+    endif;
+
+?>
--- template-parts/explore-uk.php
+++ template-parts/explore-uk.php
@@ -1,50 +1,137 @@
-<div class="container" data-country="United Kingdom">
-        <div class="row">
-            <div class="grid-item grid-30 bg-uk flex centralize">
-                    <div class="col-inner">
-                        <h1>United Kingdom</h1>
-                        <p>Beautiful destinations located within the heart of Barcelona and Madrid</p>
-                    </div>
-                </div>
-                <div class="grid-item third bg-uk-image flex centralize" style="background-image: url('<?php bloginfo('stylesheet_directory') ?>/dist/img/411x364_Edinburgh.png')">
-                    <h3 class="underline-blue">Edinburgh</h3>
-                </div>
-                <div class="grid-item grid-375 vertical-split no-padding">
-                    <div class="vertical-half offers">
-                        <a href="#">
-                            <img src="<?php bloginfo('stylesheet_directory') ?>/dist/img/pounds.png" alt=""> See offers </a>
-                    </div>
-                    <div class="vertical-half offers">
-                        <a href="#">
-                            <img src="<?php bloginfo('stylesheet_directory') ?>/dist/img/things-icon.png" alt=""> #Thingstodo</a>
-                    </div>
-                </div>
-        </div>
-        <div class="slider-wrapper">
-            <div class="row explore-slider" style="height: 34rem">
-                <div class="grid-item half bg-spain-image flex centralize" style="background-image: url('<?php bloginfo('stylesheet_directory') ?>/dist/img/600x329_Elephant.png')">
-                    <h3 class="underline-blue">Elephant &amp Castle</h3>
-                </div>
-                <div class="grid-item grid-225 bg-spain-image flex centralize" style="background-image: url('<?php bloginfo('stylesheet_directory') ?>/dist/img/269x329_York.png')">
-                    <h3 class="underline-blue">York</h3>
-                </div>
-                <div class="grid-item grid-275 bg-spain-image flex centralize no-margin-right" style="background-image: url('<?php bloginfo('stylesheet_directory') ?>/dist/img/349x329_Holland_Park.png')">
-                    <h3 class="underline-blue">Holland Park</h3>
-                </div>
-            </div>
-        </div>
+<?php 
 
-        <div class="slider-controls">
+    $args = array(
+        'taxonomy' => 'locations',
+        'hide_empty' => false,
+    );
+    $terms = get_terms($args);
 
-        </div>
-        <div class="drag-info">
-            <div class="drag-img">
-                <img src="<?php bloginfo('stylesheet_directory') ?>/dist/img/drag-img.png" alt="">
-            </div>
-            <div class="drag-text">
-                <p>
-                    Drag to Explore
-                </p>
-            </div>
-        </div>
-    </div>
+    if ( $terms && !is_wp_error( $terms ) ) :
+        $i = 0;
+        foreach ( $terms as $term ) :
+            if ( $term->slug == "uk" ) {
+
+                ?>
+                    
+
+                <div class="container" data-country="United Kingdom">
+                    <div class="row">
+                        <div class="grid-item grid-30 bg-uk flex centralize">
+                                <div class="col-inner">
+                                    <h1><?php echo $term->name ?></h1>
+                                    <p><?php echo $term->description ?></p>
+                                </div>
+                            </div>
+                            <?php 
+                            
+                                $query = new WP_Query( array(
+                                    'post_type' => 'hostel',
+                                    'posts_per_page' => 1,
+                                    'tax_query' => array(
+                                        array(
+                                            'taxonomy' => 'locations',
+                                            'field' => 'slug',
+                                            'terms' => $term->slug
+                                        )
+                                    )
+                                ) );
+
+                                if ( $query->have_posts() ) :
+                                    while ( $query->have_posts() ) : $query->the_post();
+                                        ?>
+
+                                        <div class="grid-item third bg-uk-image flex centralize" style="background-image: url('<?php echo get_the_post_thumbnail_url() ?>')">
+                                            <h3 class="underline-blue"><?php the_title() ?></h3>
+                                        </div>
+
+                                        <?php
+                                    endwhile;
+                                endif;
+
+                            ?>
+                            
+                            <div class="grid-item grid-375 vertical-split no-padding">
+                                <div class="vertical-half offers">
+                                    <a href="#">
+                                        <img src="<?php bloginfo('stylesheet_directory') ?>/dist/img/pounds.png" alt=""> See offers </a>
+                                </div>
+                                <div class="vertical-half offers">
+                                    <a href="#">
+                                        <img src="<?php bloginfo('stylesheet_directory') ?>/dist/img/things-icon.png" alt=""> #Thingstodo</a>
+                                </div>
+                            </div>
+                    </div>
+                    <div class="slider-wrapper">
+                        <div class="row explore-slider" style="height: 34rem">
+
+                            <?php
+                            
+                                $query = new WP_Query( array(
+                                    'post_type' => 'hostel',
+                                    'tax_query' => array(
+                                        array(
+                                            'taxonomy' => 'locations',
+                                            'field' => 'slug',
+                                            'terms' => $term->slug
+                                        )
+                                    )
+                                ) );
+                                
+                                if ( $query->have_posts() ) :
+                                    $i = 0;
+                                    while( $query->have_posts() ) : $query->the_post();
+                                        if ($i == 0) {
+                                            echo '';
+                                        } else {
+                                            echo '
+                                            <div class="grid-item ' . get_field('size_in_slider') . ' bg-spain-image flex centralize" style="background-image: url(' . get_the_post_thumbnail_url() . ')">
+                                                <h3 class="underline-blue">' . get_the_title() . '</h3>
+                                            </div>';
+                                        }
+                                        $i++;
+                                    endwhile;
+                                endif;
+                            
+                            ?>
+
+
+                            <!-- <div class="grid-item half bg-spain-image flex centralize" style="background-image: url('<?php bloginfo('stylesheet_directory') ?>/dist/img/600x329_Elephant.png')">
+                                <h3 class="underline-blue">Elephant &amp Castle</h3>
+                            </div>
+                            <div class="grid-item grid-225 bg-spain-image flex centralize" style="background-image: url('<?php bloginfo('stylesheet_directory') ?>/dist/img/269x329_York.png')">
+                                <h3 class="underline-blue">York</h3>
+                            </div>
+                            <div class="grid-item grid-275 bg-spain-image flex centralize no-margin-right" style="background-image: url('<?php bloginfo('stylesheet_directory') ?>/dist/img/349x329_Holland_Park.png')">
+                                <h3 class="underline-blue">Holland Park</h3>
+                            </div> -->
+                        </div>
+                    </div>
+
+                    <div class="slider-controls">
+
+                    </div>
+                    <div class="drag-info">
+                        <div class="drag-img">
+                            <img src="<?php bloginfo('stylesheet_directory') ?>/dist/img/drag-img.png" alt="">
+                        </div>
+                        <div class="drag-text">
+                            <p>
+                                Drag to Explore
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <?php
+
+            }
+            ?>
+            <?php
+            $i++;
+        endforeach;
+    endif;
+
+?>
+
